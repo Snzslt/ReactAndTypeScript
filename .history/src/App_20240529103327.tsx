658773eb46cd5c5b22ac8ac772c79054f0aa0ba9
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-05-29 10:34:30 -0400

    removing todo

--- .history/src/App_20240529103327.tsx
+++ .history/src/App_20240529103327.tsx
@@ -1,13 +1,4 @@
 
-  import { useState } from 'react';
-
-  import NewTodo from './components/NewTodo';
-  import Todos from './components/Todos';
-  import Todo from './models/todo';
-  
-  function App() {
-    const [todos, setTodos] = useState<Todo[]>([]);
-  
   const addTodoHandler = (todoText: string) => {
     const newTodo = new Todo(todoText);
 
--- .history/src/App_20240529103347.tsx
+++ .history/src/App_20240529103347.tsx
@@ -1,12 +1,5 @@
 
-  import { useState } from 'react';
-
-  import NewTodo from './components/NewTodo';
-  import Todos from './components/Todos';
-  import Todo from './models/todo';
   
-  function App() {
-    const [todos, setTodos] = useState<Todo[]>([]);
   
   const addTodoHandler = (todoText: string) => {
     const newTodo = new Todo(todoText);
