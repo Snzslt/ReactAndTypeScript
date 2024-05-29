31f08746ae5c9f39dd3da7e206ec184ba1bc9766
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-05-29 09:39:33 -0400

    adding a data model

--- .history/src/App_20240529092659.tsx
+++ .history/src/App_20240529092659.tsx
@@ -2,7 +2,9 @@ import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+  const todos = 
+  [new Todo('Learn React'), 
+  new Todo('Learn TypeScript')];
 
   return (
     <div>
--- .history/src/components/Todos_20240529093804.tsx
+++ .history/src/components/Todos_20240529093804.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Todo from '../models/todo';
-import TodoItem from './TodoItem';
+
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text}/>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
